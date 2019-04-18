5b62948bc35d1d5f5f82bfe0eed1c4e65973ab76
Ian Walter <public@iankwalter.com>
2019-04-17 21:09:45 -0400

    Updating bff

--- tests/cli.tests.js
+++ tests/cli.tests.js
@@ -3,6 +3,5 @@ const execa = require('execa')
 
 test('cli', async ({ expect }) => {
   const { stdout } = await execa('./tests/helpers/exampleCli.js', ['-c', '4'])
-  // TODO: replace with snapshot when it's added to bff.
-  expect(stdout).toContain(`level: 'debug', concurrency: 4`)
+  expect(stdout).toMatchSnapshot()
 })
