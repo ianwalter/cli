10fc9cc537f9b8f98b96bc40b2d4f557f9db4ed5
Ian Walter <public@iankwalter.com>
2019-04-19 13:33:38 -0400

    Fixing dotprop bug by replacing Object.assign with @ianwalter/merge

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const pkgConf = require('pkg-conf')
 const getopts = require('getopts')
 const dotProp = require('dot-prop')
+const merge = require('@ianwalter/merge')
 
 module.exports = function cli ({ name, opts }) {
   // Create the configuration object that will be returned.
@@ -29,7 +30,7 @@ module.exports = function cli ({ name, opts }) {
 
   // Add/overwrite configuration data with options passed through command-line
   // flags.
-  Object.assign(config, cliOpts)
+  merge(config, cliOpts)
 
   // Return the populated configuration object.
   return config
