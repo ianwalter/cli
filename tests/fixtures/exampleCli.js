f9550eef7f2fb169d586eecf6e21b9c0dff41574
Ian Walter <public@iankwalter.com>
2019-10-29 00:28:27 -0400

    Working on new API for options

--- tests/fixtures/exampleCli.js
+++ tests/fixtures/exampleCli.js
@@ -4,7 +4,16 @@ const cli = require('../..')
 
 const config = cli({
   name: 'exampleCli',
-  opts: { alias: { concurrency: 'c' } }
+  options: {
+    concurrency: {
+      alias: 'c',
+      description: `
+        The number of CPU cores to use when executing the application. The value
+        must be an integer between 1 and 4.
+      `,
+      default: 1
+    }
+  }
 })
 
 config.$package = { name: config.$package.name }
